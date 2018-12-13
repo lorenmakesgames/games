d470deec72bc423323ab3bac5106be8c3d328ecf
Loren Heubert-Aubry <lorenha@umich.edu>
2018-12-12 23:22:12 -0500

    minor style changes

--- js/games.js
+++ js/games.js
@@ -1,10 +1,13 @@
 $(document).ready(function(){
     $(".card_image img").click(function(){
         console.log("clicked");
-        $(this).closest(".game_card").find(".modal").toggleClass("modalActive");
+        $(".modal").removeClass("modalActive");
+        $(this).closest(".game_card").find(".modal").addClass("modalActive");
+        $(this).closest(".game_card").find(".modal").draggable();
+
     });
 
     $(".close").click(function(){
-        $(this).closest(".modal").toggleClass("modalActive");
+        $(this).closest(".modal").removeClass("modalActive");
     });
 });
